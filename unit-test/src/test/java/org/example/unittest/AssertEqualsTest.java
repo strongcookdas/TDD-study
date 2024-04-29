00e885972e56d45a97d7bc29d129e173bd0d8478
strongcookdas <goodjav141592@gmail.com>
2024-04-29 15:11:28 +0900

    chore : assertThrows, fail 메서드 실습

--- unit-test/src/test/java/org/example/unittest/AssertEqualsTest.java
+++ unit-test/src/test/java/org/example/unittest/AssertEqualsTest.java
@@ -1,14 +1,41 @@
 package org.example.unittest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertEqualsTest {
     @Test
-    void assertEqualsExample1Test(){
+    void assertEqualsExample1Test() {
         int actual = 2;
         int expected = 2;
         assertEquals(expected, actual);
+    }
+
+    private String toUpperCase(String name) throws NullPointerException {
+        if (name == null) throw new NullPointerException();
+        return name.toUpperCase();
+    }
+
+    @Test
+    void toUpperCaseThrowNullPointExceptionTrueTest() {
+        assertThrows(NullPointerException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                toUpperCase(null);
+            }
+        });
+    }
+
+    @Test
+    void toUpperCaseThrowNullPointExceptionTest() {
+        try {
+            String name = toUpperCase("JIMIN");
+            fail();
+            assertEquals("JIMIN", name);
+        } catch (NullPointerException e) {
+
+        }
     }
 }
