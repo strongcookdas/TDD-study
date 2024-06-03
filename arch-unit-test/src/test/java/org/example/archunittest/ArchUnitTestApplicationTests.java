69c1b3264d30b906302a722b2cb22cd6c50ddc0c
strongcookdas <goodjav141592@gmail.com>
2024-06-03 15:30:07 +0900

    chore : architecture unit test 실습 프로젝트 생성 및 실습

---
+++ arch-unit-test/src/test/java/org/example/archunittest/ArchUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.archunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
