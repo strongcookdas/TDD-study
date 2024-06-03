006e719eb360d3896f9d2fe494b9f098e9cfdb3d
strongcookdas <goodjav141592@gmail.com>
2024-06-03 14:07:03 +0900

    chore : chaos monkey 실습 프로젝트 생성 및 진행

---
+++ chaos-monkey/src/main/java/org/example/chaosmonkey/ChaosMonkeyApplication.java
@@ -0,0 +1,13 @@
+package org.example.chaosmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosMonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaosMonkeyApplication.class, args);
+	}
+
+}
---
+++ chaos-monkey/src/test/java/org/example/chaosmonkey/ChaosMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chaosmonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
