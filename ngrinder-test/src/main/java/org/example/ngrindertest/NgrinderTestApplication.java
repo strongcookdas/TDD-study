ede2a58bf6d4e0701caad097b8866b10e04ba1a0
strongcookdas <goodjav141592@gmail.com>
2024-06-03 10:43:03 +0900

    chore : nGrinder test 프로젝트 생성

---
+++ ngrinder-test/src/main/java/org/example/ngrindertest/NgrinderTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.ngrindertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgrinderTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgrinderTestApplication.class, args);
+	}
+
+}
---
+++ ngrinder-test/src/test/java/org/example/ngrindertest/NgrinderTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ngrindertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgrinderTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
